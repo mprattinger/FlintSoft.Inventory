7d47e0e0a89f6520cec6ba70921144374255a7c1
mprattinge <michael.prattinger@becom-group.com>
2024-09-18 16:08:22 +0200

    wip

---
+++ src/FlintSoft.Inventory.Domain/Storage/Storage.cs
@@ -0,0 +1,14 @@
+﻿using FlintSoft.Inventory.Domain.Common.Models;
+using FlintSoft.Inventory.Domain.Storage.ValueObjects;
+
+namespace FlintSoft.Inventory.Domain.Storage;
+
+public sealed class Storage : AggregateRoot<StorageId>
+{
+    private readonly List<Yard> _yards = [];
+    public string Name { get; }
+
+    public string Description { get; }
+
+    public readonly IReadOnlyList<Yard> Yards => _yards.AsReadOnly();
+}
---
+++ src/FlintSoft.Inventory.Domain/Storage/ValueObjects/StorageId.cs
@@ -0,0 +1,23 @@
+﻿using FlintSoft.Inventory.Domain.Common.Models;
+
+namespace FlintSoft.Inventory.Domain.Storage.ValueObjects;
+
+public sealed class StorageId : ValueObject
+{
+    public Guid Value { get; }
+
+    private StorageId(Guid value)
+    {
+        Value = value;
+    }
+
+    public static StorageId CreateUnique()
+    {
+        return new(Guid.NewGuid());
+    }
+
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+}
